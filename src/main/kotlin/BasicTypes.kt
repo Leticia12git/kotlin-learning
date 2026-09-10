f314481eba12fab3f88874bb4ec1dd9a1a55f23e
Leticia Lima <leticia_lele.lima@hotmail.com>
2026-09-09 22:14:31 -0300

    Up learning-kotlin

---
+++ src/main/kotlin/BasicTypes.kt
@@ -0,0 +1,24 @@
+package com.leticia
+
+class BasicTypes {
+
+    var customers = 10
+    customers = 0
+    customers = customer + 3
+    customers += 7
+    customers -= 3
+    customers /3
+    println (customers)
+
+    val d: Int
+
+    d = 3
+    val e: String = "hello"
+
+    println(d)
+    println(e)
+
+    val d: Int
+
+    println(d)
+}
